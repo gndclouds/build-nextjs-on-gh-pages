d28bb3d3f5248a98b6f95ecc30ab895d26444229
Will Felker <gndclouds@icloud.com>
2024-02-05 12:08:16 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
