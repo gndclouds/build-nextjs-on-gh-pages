e8101782cdd3dae01f586e3ed218b212f3d519da
William Felker <gndclouds@icloud.com>
2024-02-05 09:58:54 -0800

    cleaning

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
